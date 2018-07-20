325b23c097959b622903aa6537e34ff7e9baeae7
Benjamin <benjaminsalasek@Athena.local>
2018-07-20 14:43:49 -0500

    I'm giving up now.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,17 +5,60 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { catchError, map, tap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+	private heroesUrl = 'api/heroes';
+
+
+
+  constructor(
+	private http: HttpClient,
+	private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+	  this.log('fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      catchError(this.handleError('getHeroes', []))
+    );
   }
+
+	getHero(id: number): Observable<Hero> {
+  // TODO: send the message _after_ fetching the hero
+		this.log(`fetched hero id=${id}`)
+  return of(HEROES.find(hero => hero.id === id));
+}
+
+	private log(message: string) {
+  		this.messageService.add(`HeroService: ${message}`);
+	}
+
+
+	/** PUT: update the hero on the server */
+updateHero (hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+  );
+}
+
+	private handleError<T> (operation = 'operation', result?: T) {
+  		return (error: any): Observable<T> => {
+			console.error(error);
+			this.log(`${operation} failed: ${error.message}`);
+			return of(result as T);
+		};
+	}
 }
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HeroService } from './hero.service';
 
 @Injectable({
   providedIn: 'root'
